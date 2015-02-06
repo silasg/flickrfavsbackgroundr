be5b965dd3f681f5473741e876f82555c077cba2
Silas Graffy <silas@Silas-MBP.fritz.box>
2015-02-06 18:23:46 +0100

    Initial Commit

---
+++ FlickrFavsBackgroundR/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FlickrFavsBackgroundR
+//
+//  Created by Silas Graffy on 06/02/15.
+//  Copyright (c) 2015 Silas Graffy. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
