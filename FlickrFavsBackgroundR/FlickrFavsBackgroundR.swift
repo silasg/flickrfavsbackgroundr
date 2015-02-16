f2d87a857b0faf0180ae6bd347374555bf82c258
Silas Graffy <silas@Silas-MBP.fritz.box>
2015-02-16 20:59:35 +0100

    refactoring

---
+++ FlickrFavsBackgroundR/FlickrFavsBackgroundR.swift
@@ -0,0 +1,73 @@
+//
+//  FlickrFavsBackgroundR.swift
+//  FlickrFavsBackgroundR
+//
+//  Created by Silas Graffy on 16/02/15.
+//  Copyright (c) 2015 Silas Graffy. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+public class FlickFavsBackgroudR {
+
+    let flickrApiKey: String
+    let flickrUserId: String
+    let flickrFetchCount: Int
+    let sem = dispatch_semaphore_create(0)
+
+    init(flickrApiKey: String, flickrUserId: String, flickrFetchCount: Int = 100) {
+        self.flickrApiKey = flickrApiKey
+        self.flickrUserId = flickrUserId
+        self.flickrFetchCount = flickrFetchCount
+    }
+    
+    public func go() {
+        let flickrFavsDelegate = FlickrFavsParserDelegate()
+        let xmlParser = createParser(flickrFavsDelegate)
+        println("Getting and parsing Favorite Photos of Flickr User '\(flickrUserId)' with Size 'k' (2048 px min)...")
+        xmlParser.parse()
+        let urls = flickrFavsDelegate.getUrls()
+        println("\(urls.count) Photos found.")
+        
+        if let url = urls.first {
+            println("Downloading first Photo from \(url) ...")
+            let downloadTask = NSURLSession.sharedSession().downloadTaskWithURL(NSURL(string: url)!,
+                completionHandler: setWallpaperToDownloadResult)
+            downloadTask.resume()
+        }
+        
+        dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER)
+    }
+    
+    private func createParser(delegate: NSXMLParserDelegate) -> NSXMLParser {
+        let flickrRestUrl: String = "https://api.flickr.com/services/rest/?method=flickr.favorites.getPublicList&api_key=\(flickrApiKey)&user_id=\(flickrUserId)&per_page=\(flickrFetchCount)&extras=url_k"
+        let flickrFavsUrl = NSURL(string: flickrRestUrl)
+        let xmlParser = NSXMLParser(contentsOfURL: flickrFavsUrl)!
+        xmlParser.delegate = delegate
+        return xmlParser
+    }
+    
+    private func setWallpaperToDownloadResult(location: NSURL!, response: NSURLResponse!, error: NSError!) {
+        if let err = error {
+            println("!! Downloading Image failed: \(err.localizedDescription)")
+        } else {
+            setWallpaperToImageFile(location)
+        }
+    }
+    
+    private func setWallpaperToImageFile(imgUrl: NSURL) {
+        var error : NSError?
+        let workspace = NSWorkspace.sharedWorkspace()
+        let mainScreen = NSScreen.mainScreen()!
+        
+        if workspace.setDesktopImageURL(imgUrl, forScreen: mainScreen, options: nil, error: &error) {
+            println("Desktop Background successfully set to \(imgUrl.description).")
+        } else {
+            println("!! Setting Desktop Background failed: \(error!.localizedDescription)")
+        }
+        dispatch_semaphore_signal(sem);
+        
+    }
+
+}
--- FlickrFavsBackgroundR/main.swift
+++ FlickrFavsBackgroundR/main.swift
@@ -7,59 +7,10 @@
 //
 
 import Foundation
-import Cocoa
-import Appkit
-
 
 let flickrApiKey = "794d6625970fac78530fb593c46d7a34"
 
 let flickrUserId = "66956608@N06"
 
-let flickrFetchCount = 100
-
-let flickrRestUrl = "https://api.flickr.com/services/rest/?method=flickr.favorites.getPublicList&api_key=\(flickrApiKey)&user_id=\(flickrUserId)&per_page=\(flickrFetchCount)&extras=url_k"
-
-let sem = dispatch_semaphore_create(0)
-
-func setDesktopBackground(imgUrl: NSURL) {
-    var error : NSError?
-    var workspace = NSWorkspace.sharedWorkspace()
-    var screen = NSScreen.mainScreen()!
-    
-    if workspace.setDesktopImageURL(imgUrl, forScreen: screen, options: nil, error: &error) {
-        println("Desktop Background successfully set to \(imgUrl.description).")
-    } else {
-        println("!! Setting Desktop Background failed: \(error!.localizedDescription)")
-    }
-    dispatch_semaphore_signal(sem);
-    
-}
-
-func downloadCompleted(location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void {
-    if let err = error {
-        println("!! Downloading Image failed: \(err.localizedDescription)")
-    } else {
-        setDesktopBackground(location)
-    }
-}
-
-
-let flickrFavsUrl = NSURL(string: flickrRestUrl)
-
-let flickrFavsDelegate = FlickrFavsParserDelegate()
-var xmlParser = NSXMLParser(contentsOfURL: flickrFavsUrl)!
-xmlParser.delegate = flickrFavsDelegate
-println("Getting and parsing Favorite Photos of Flickr User '\(flickrUserId)' with Size 'k' (2048 px min)...")
-xmlParser.parse()
-let urls = flickrFavsDelegate.getUrls()
-println("\(urls.count) Photos found.")
-if let url = urls.first {
-    println("Downloading first Photo from \(url) ...")
-    let downloadTask = NSURLSession.sharedSession().downloadTaskWithURL(NSURL(string: url)!,
-        completionHandler: downloadCompleted)
-    
-    downloadTask.resume()
-
-}
-
-dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER)
+let backgroundR = FlickFavsBackgroudR(flickrApiKey: flickrApiKey, flickrUserId: flickrUserId)
+backgroundR.go()
